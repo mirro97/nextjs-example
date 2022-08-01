9ac6a62359744e193a5d0d19469943e309d08267
sujeong.k <45249021+mirro97@users.noreply.github.com>
2022-08-02 00:41:38 +0900

    module css 예제 업로드

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,23 +1,31 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+// css 모듈 사용하기
+import styles from "./NavBar.module.css";
+
 export default function NavBar() {
   const router = useRouter();
 
   console.log(router); // 이렇게 콘솔에 찍으면
 
   return (
-    <nav>
+    <nav className={styles.nav}>
       <Link href="/">
         <a
-          style={{ color: router.pathname === "/" ? "red" : "orange" }}
-          className="red"
+          className={`
+          ${styles.under} ${router.pathname === "/" ? styles.active : ""}`}
         >
-          home
+          HOME
         </a>
       </Link>
       <Link href="/farm">
-        <a style={{ color: router.pathname === "/farm" ? "red" : "orange" }}>
+        <a
+          className={[
+            styles.under,
+            router.pathname === "/farm" ? styles.active : "",
+          ].join(" ")}
+        >
           MY FARM
         </a>
       </Link>
